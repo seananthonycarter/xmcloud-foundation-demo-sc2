e885f142a1acbf11eb27063ba68c7e54e2529e9c
Sean Carter <sean.carter@sitecore.com>
2024-03-26 17:56:59 +0000

    Content updated from XMC

---
+++ src/sxademositecollection2/sxademosite1/src/components/SXA Demo Page Navigation.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Field, Text } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  NavTitle: Field<string>;
+}
+
+type NavigationProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const SXADemoPageNavigationDefaultComponent = (props: NavigationProps): JSX.Element => (
+  <div className={`component my-component ${props.params.styles}`}>
+    <h1>ZZZZ</h1>
+    <div className="component-content">
+    <span className="is-empty-hint">No Datasource Present</span>
+    </div>
+  </div>
+);
+
+export const Default = (props: NavigationProps): React.JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  if (props.fields) {
+    return (
+      <div className={`component my-component ${props.params.styles}`}
+        id={id ? id : undefined}>
+        <Text className="text-white bg-black" field={props.fields.NavTitle} />
+      </div>
+    );
+  }
+
+  return <SXADemoPageNavigationDefaultComponent {...props} />;
+};
+
+
+export const NegativeDisplay = (props: NavigationProps): React.JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  if (props.fields) {
+    return (
+      <div
+        className={`component my-component text-white bg-black ${props.params.styles}`}
+        id={id ? id : undefined}
+      >
+        <Text className="text-white bg-black" field={props.fields.NavTitle} />
+      </div>
+    );
+  }
+
+  return <SXADemoPageNavigationDefaultComponent {...props} />;
+};
