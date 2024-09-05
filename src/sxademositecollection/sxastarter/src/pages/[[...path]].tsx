99636254e3a67d8c64b864699b185108733ca2a2
Sean Carter <sean.carter@sitecore.com>
2024-09-05 14:07:19 +0100

    amended revalidate

--- src/sxademositecollection/sxastarter/src/pages/[[...path]].tsx
+++ src/sxademositecollection/sxastarter/src/pages/[[...path]].tsx
@@ -98,7 +98,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 5 seconds
-    revalidate: 5, // In seconds
+    revalidate: 120, // In seconds
     notFound: props.notFound, // Returns custom 404 page with a status code of 404 when true
   };
 };
