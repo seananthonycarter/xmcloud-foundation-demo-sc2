1fab180f622aef7ee6c709914cd2a77f55a410b3
Sean Carter <sean.carter@sitecore.com>
2024-09-27 10:48:42 +0100

    no message

--- src/sxademositecollection/sxastarter/src/pages/api/testroute.ts
+++ src/sxademositecollection/sxastarter/src/pages/api/testroute.ts
@@ -3,6 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function Hndler(req: NextApiRequest, res: NextApiResponse) {
 
   if (req.method === 'GET') {
-    res.status(200).json({ name: 'Hello world from the API' })
+    res.status(200).json({ name: 'Hello world from the API' });
   }
 }
