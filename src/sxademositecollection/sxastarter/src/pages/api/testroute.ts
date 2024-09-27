4f7582662f5418e4c056bd5ce12484d528c38501
Sean Carter <sean.carter@sitecore.com>
2024-09-27 11:16:07 +0100

    Linting pain

--- src/sxademositecollection/sxastarter/src/pages/api/testroute.ts
+++ src/sxademositecollection/sxastarter/src/pages/api/testroute.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function Handler(req: NextApiRequest, res: NextApiResponse) {
-
   if (req.method === 'GET') {
     res.status(200).json({ name: 'Hello world from the API' });
   }
